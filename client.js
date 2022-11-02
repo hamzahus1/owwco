b598b9f3c4f9c076204834762571bcb9a2ed829e
ittid <ahnaine3bd@gmail.com>
2022-11-02 15:57:54 +0100

    fix css code and html semantic and make the project more flexible

---
+++ client.js
@@ -0,0 +1,13 @@
+/*=== === === === === === === === === === === === === === === === === === 
+                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                  ~~~ OWWCO WEB AGENCY Project ~~~
+                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Author: https://github.com/ittid - https://github.com/ihamzahus1
+    Email: 
+    Release Date: 2020 - updated 2 nov, 2022
+    Detail: A React and Project
+=== === === === === === === === === === === === === === === === === ===*/
+
+addEventListener("DOMContentLoaded", () => {
+  console.log("Hello World!");
+});
